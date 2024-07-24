441a86e1ca78d71ebeddc85b8081defe847ea608
Santerson <santacap2019@gmail.com>
2024-07-24 16:22:23 -0700

    Fat commit but mildly bugged (i'll fix later)

---
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void Awake()
+    {
+        DontDestroyOnLoad[] objs = FindObjectsOfType<DontDestroyOnLoad>();
+
+        if (objs.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
