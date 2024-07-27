71c0819da761b41dd07d99f884addc041bbcac50
Santerson <santacap2019@gmail.com>
2024-07-27 14:44:53 -0700

    Added trash can

--- Assets/Scripts/Extensions/SUtilities.cs
+++ Assets/Scripts/Extensions/SUtilities.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public static class SUtilities
 {
+    /// <summary>
+    /// Checks if a position is in a rectangle drawn from a bottom left and top right corner (inclusive)
+    /// </summary>
+    /// <param name="position">The point of the object</param>
+    /// <param name="bottomLeft">Bottom left of the rectangle</param>
+    /// <param name="topRight">Top right of the rectangle</param>
+    /// <returns>Whether or not the point is in the rectangle</returns>
     public static bool IsInRange(Vector3 position, Vector3 bottomLeft, Vector3 topRight)
     {
         return (position.x >= bottomLeft.x && position.x <= topRight.x && position.y >= bottomLeft.y && position.y <= topRight.y);
---
+++ Assets/Scripts/TrashCan.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TrashCan : MonoBehaviour
+{
+    [SerializeField] Vector2 DetectionBottomLeft;
+    [SerializeField] Vector2 DetectionTopRight;
+    [SerializeField] TextMeshProUGUI text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        DebugExtensions.DrawBox(DetectionBottomLeft, DetectionTopRight, Color.red);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerPos = FindObjectOfType<PlayerMovement>().transform.position;
+        if (FindObjectOfType<Pickup>().Holding && SUtilities.IsInRange(playerPos, DetectionBottomLeft, DetectionTopRight)) FadeIn();
+        else { FadeOut(); }
+        if (SUtilities.IsInRange(playerPos, DetectionBottomLeft, DetectionTopRight))
+        {
+            if (FindObjectOfType<Pickup>().Holding && Input.GetKey(KeyCode.Space))
+            {
+                FindObjectOfType<Pickup>().Holding = false;
+                FindObjectOfType<TakeDepositOrders>().ObjectHolding.GetComponent<PickupableObjects>().DropByPlayer();
+            }
+        }
+    }
+
+    void FadeOut()
+    {
+        if (text.color.a <= 0)
+        {
+            return;
+        }
+        text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - 0.01f);
+
+    }
+
+    void FadeIn()
+    {
+        if (text.color.a >= 1)
+        {
+            return;
+        }
+        text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + 0.01f);
+
+    }
+}
