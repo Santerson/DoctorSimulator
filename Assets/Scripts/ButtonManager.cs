1ff8b137994a584ca984445a284c914f667ca342
Santerson <santacap2019@gmail.com>
2024-07-29 15:59:41 -0700

    added button.mp3

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -6,26 +6,30 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
 
-    
    public void GoToMainScene()
     {
+
         SceneManager.LoadScene("Main");
     }
     public void GoToCredits()
     {
+
         SceneManager.LoadScene("Credits");
     }
     public void GoToStart()
     {
+
         SceneManager.LoadScene("Start");
     }
     public void GoToStartFromEnd()
     {
+
         FindObjectOfType<FinalScore>().deleteText();
         SceneManager.LoadScene("Start");
     }
     public void GoToTips()
     {
+
         SceneManager.LoadScene("Instructions");
     }
 }
