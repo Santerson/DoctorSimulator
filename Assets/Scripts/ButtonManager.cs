a877a9b311d34f6ec1a344f5e79140726f6d2038
Santerson <santacap2019@gmail.com>
2024-07-31 17:02:30 -0700

    ADDED BUTTON CLICK SFX!!!!

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -10,34 +10,55 @@ public class ButtonManager : MonoBehaviour
     {
         try
         {
-            FindObjectOfType<>
+            FindObjectOfType<SoundEffectPlayer>().PlayClickSound();
         }
         catch { }
         SceneManager.LoadScene("Main");
     }
     public void GoToCredits()
     {
-
+        try
+        {
+            FindObjectOfType<SoundEffectPlayer>().PlayClickSound();
+        }
+        catch { }
         SceneManager.LoadScene("Credits");
     }
     public void GoToStart()
     {
-
+        try
+        {
+            FindObjectOfType<SoundEffectPlayer>().PlayClickSound();
+        }
+        catch { }
         SceneManager.LoadScene("Start");
     }
     public void GoToStartFromEnd()
     {
-
+        try
+        {
+            FindObjectOfType<SoundEffectPlayer>().PlayClickSound();
+        }
+        catch { }
         FindObjectOfType<FinalScore>().deleteText();
         SceneManager.LoadScene("Start");
     }
     public void GoToTips()
     {
-
+        try
+        {
+            FindObjectOfType<SoundEffectPlayer>().PlayClickSound();
+        }
+        catch { }
         SceneManager.LoadScene("Tips");
     }
     public void QuitToDesktop()
     {
+        try
+        {
+            FindObjectOfType<SoundEffectPlayer>().PlayClickSound();
+        }
+        catch { }
         Application.Quit();
     }
 }
--- Assets/Scripts/SoundEffectPlayer.cs
+++ Assets/Scripts/SoundEffectPlayer.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class SoundEffectPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] AudioSource ButtonClick;
+    public void PlayClickSound()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        ButtonClick.Play();
     }
 }
