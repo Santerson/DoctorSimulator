9d375bf3510d2f72ecc5aff1a369cd038c377851
Santerson <santacap2019@gmail.com>
2024-08-01 15:14:32 -0700

    Pause button fully done

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -35,6 +35,17 @@ public class Pause : MonoBehaviour
         GamePaused = true;
         transform.position = Vector2.zero;
     }
+
+    public void PauseButton()
+    {
+        try
+        {
+            FindObjectOfType<SoundEffectPlayer>().PlayClickSound();
+        }
+        catch { Debug.LogError("Play from the start for sound effects!"); }
+        PauseGame();
+    }
+
     public void ResumeGame()
     {
         GamePaused = false;
