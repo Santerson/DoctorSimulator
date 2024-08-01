a748121f7a138f093a8d24611c063f37103f35be
Santerson <santacap2019@gmail.com>
2024-08-01 14:39:28 -0700

    started pause system

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public bool GamePaused = false;
+    Vector2 Position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!GamePaused)
+            {
+                GamePaused = true;
+                PauseGame();
+            }
+            else
+            {
+                GamePaused = false;
+                ResumeGame();
+            }
+        }
+    }
+
+
+    private void PauseGame()
+    {
+        transform.position = Vector2.zero;
+
+    }
+    private void ResumeGame()
+    {
+        throw new NotImplementedException();
+    }
+}
